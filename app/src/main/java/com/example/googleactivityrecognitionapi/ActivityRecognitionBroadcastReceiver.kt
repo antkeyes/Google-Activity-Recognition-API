365680ad3e6a686078f8dbfe299e1866a0f6e844
antKeyes <keyes.anthony@gmail.com>
2023-03-19 03:15:59 -0400

    Update receiver in manifest

--- app/src/main/java/com/example/googleactivityrecognitionapi/ActivityRecognitionBroadcastReceiver.kt
+++ app/src/main/java/com/example/googleactivityrecognitionapi/ActivityRecognitionBroadcastReceiver.kt
@@ -3,24 +3,27 @@ package com.example.googleactivityrecognitionapi
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.gms.location.ActivityTransition
-import com.google.android.gms.location.ActivityTransitionEvent
 import com.google.android.gms.location.ActivityTransitionResult
 
 class ActivityRecognitionBroadcastReceiver : BroadcastReceiver() {
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-        if (ActivityTransitionResult.hasResult(intent)) {
+    companion object {
+        const val ACTION_PROCESS_UPDATES = "com.example.activityrecognitionapp.ACTION_PROCESS_UPDATES"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == ACTION_PROCESS_UPDATES) {
             val result = ActivityTransitionResult.extractResult(intent)
-            for (event in result!!.transitionEvents) {
-                when (event.transitionType) {
-                    ActivityTransition.ACTIVITY_TRANSITION_ENTER -> {
-                        Toast.makeText(context, "Device picked up", Toast.LENGTH_SHORT).show()
-                    }
-                    ActivityTransition.ACTIVITY_TRANSITION_EXIT -> {
-                        Toast.makeText(context, "Device put down", Toast.LENGTH_SHORT).show()
-                    }
+            result?.let {
+                for (event in it.transitionEvents) {
+                    val activityType = event.activityType
+                    val activityTransition = event.transitionType
+                    val localIntent = Intent("activity-update")
+                    localIntent.putExtra("type", activityType)
+                    localIntent.putExtra("transition", activityTransition)
+                    LocalBroadcastManager.getInstance(context).sendBroadcast(localIntent)
                 }
             }
         }
